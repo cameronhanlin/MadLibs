adf995a8c59751bed91527c1134c5d6e1979359a
Cameron <cameronhanlin@Camerons-MacBook-Pro.local>
2022-02-21 13:04:19 -0700

    Refractored program for ArrayLists

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,21 @@
 package com.company;
 
-import java.util.Scanner;
-import java.util.Random;
+import java.util.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	/*
-	Mad Libs
-Create a new project named MadLibs
-Create a new repo in Github named MadLibs
-In InteliJ create a new project named MadLibs
-In Terminal navigate to your MadLibs project location
-In terminal, use git init
-Copy the Github SSH key for your new Github repo
-In Terminal Setup the git remote named origin by using the Github SSH key you copied earlier
-Stage and commit the init changes to your local repo
-Push changes to Github
-Create 3 size 5 arrays
-One array should have 5 single word strings, each of which is a noun.
-One array should have 5 single word strings, each of which is an adjective.
-One array should have 5 single word strings, each of which is a verb.
-The Java Util class Random has a nextInt() method that can be used to get a random number from the range of zero to the
-number passed in as a parameter.
-Write a small story that randomly gets words from each array to fill in blank points in the story based on the word type.
-Don’t forget to stage and commit changes in git as you go along
-Bonus: Get the words to fill each array by asking the user to supply them.
 
-
-	 */
         Scanner scan = new Scanner(System.in);
         Random rand = new Random();
+        List<String> nounNew = new ArrayList<>();
+        List<String> nounsNew = new ArrayList<>();
+        List<String> adjectiveNew = new ArrayList<>();
+        List<String> verbNew = new ArrayList<>();
+        List<String> verbingNew = new ArrayList<>();
+        List<String> verbPastNew = new ArrayList<>();
+        List<String> input = new ArrayList<>();
 
         String[] noun = {"scale","throne","island","cactus","bucket"};
         String[] nouns = {"playgrounds","visitors","donkeys","feasts","cannons"}; // plural noun words
@@ -38,42 +23,48 @@ Bonus: Get the words to fill each array by asking the user to supply them.
         String[] verb = {"knit","jump","kick","swim","visit"};
         String[] verbing = {"advising","choosing","excusing","losing","objecting"}; // verbs ending in -ing
         String[] verbPast = {"imagined","gathered","jumped","untied","yawned"}; // past tense of verbs
-        String[] input = new String[5];
         int place = 0;
 
-
+        //here I didnt want to remake my Arrays, so I just converted them to ListArrays
+        for(int i=0; i < noun.length; i++) // noun
+            nounNew.add(noun[1]);
+        for(int i=0; i < nouns.length; i++) // nouns
+            nounsNew.add(nouns[1]);
+        for(int i=0; i < adjective.length; i++) //adjective
+            adjectiveNew.add(adjective[1]);
+        for(int i=0; i < verb.length; i++) // verb
+            verbNew.add(verb[1]);
+        for(int i=0; i < verbing.length; i++) //verbing
+            verbingNew.add(verbing[1]);
+        for(int i=0; i < verbPast.length; i++) // verbPast
+            verbPastNew.add(verbPast[1]);
 
         System.out.print("Please enter a type of bird: "); //bird
-        input[place] = scan.next();
-        place++;
+        input.add(scan.nextLine().toLowerCase());
         System.out.print("Please enter a room of a house: "); //room in house
-        input[place] = scan.next();
-        place++;
-        System.out.print("Please enter the name of a relative (one word): " ); // relative name
-        input[place] = scan.next();
-        place++;
+        input.add(scan.nextLine().toLowerCase());
+        System.out.print("Please enter the name of a relative: "); // relative name
+        input.add(scan.nextLine());
         System.out.print("Please enter a liquid: "); // liquid
-        input[place] = scan.next();
-        place++;
+        input.add(scan.nextLine().toLowerCase());
         System.out.print("Please enter a part of the body (plural): "); //part of body
-        input[place] = scan.next();
+        input.add(scan.nextLine().toLowerCase());
         System.out.println("\b");
 
 
-
-
-
-        System.out.println("It was a "+adjective[rand.nextInt(5)]+", cold February day.");
-        System.out.println("I woke up to the "+adjective[rand.nextInt(5)]+" smell of "+input[0]+" roasting in the "+input[1]+" downstairs.");
-        System.out.println("I "+verbPast[rand.nextInt(5)]+" down the stairs to see if I could help "+verb[rand.nextInt(5)]+" the dinner.");
-        System.out.println("My mom said,'See if "+input[2]+" needs a fresh "+noun[rand.nextInt(5)]+"'.");
-        System.out.println("So I carried a tray of glasses full of "+input[3]+" into the room");
-        System.out.println("When I got there, I couldn’t believe my "+input[4]+"!");
-        System.out.println("There were "+nouns[rand.nextInt(5)]+" "+verbing[rand.nextInt(5)]+" on the "+noun[rand.nextInt(5)]+"!");
-
-
+        //main output
+        System.out.println("It was a "+adjectiveNew.get(rand.nextInt(5))+", cold February day.");
+        System.out.println("I woke up to the "+adjectiveNew.get(rand.nextInt(5))+" smell of "+input.get(0)+" roasting in the "+input.get(1)+" downstairs.");
+        System.out.println("I "+verbPastNew.get(rand.nextInt(5))+" down the stairs to see if I could help "+verbNew.get(rand.nextInt(5))+" the dinner.");
+        System.out.println("My mom said,'See if "+input.get(2)+" needs a fresh "+nounNew.get(rand.nextInt(5))+"'.");
+        System.out.println("So I carried a tray of glasses full of "+input.get(3)+" into the room");
+        System.out.println("When I got there, I couldn’t believe my "+input.get(4)+"!");
+        System.out.println("There were "+nounsNew.get(rand.nextInt(5))+" "+verbingNew.get(rand.nextInt(5))+" on the "+nounNew.get(rand.nextInt(5))+"!");
 
 
 
     }
+
 }
+
+
